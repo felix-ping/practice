dec35d4c4d12547a4ca80b3a44f00741c1dc4868
ping <513011631@qq.com>
2018-11-23 21:45:48 +0800

    极大数加法实现

--- practices.js
+++ practices.js
@@ -1336,3 +1336,22 @@ function LazyMan(name){
 		return new LazyMan();
 	};
 })(window);
+
+function add(num1, num2) {
+    let len = Math.max(num1.length, num2.length)
+    num1 = num1.padStart(len, 0)
+    num2 = num2.padStart(len, 0)
+    let flag = 0
+    let result = ''
+    let temp = 0
+    for(let i=len-1; i>=0; i--){
+        temp = flag + parseInt(num1[i]) + parseInt(num2[i])
+        result = (temp%10) + result 
+        flag = parseInt(temp/10)
+    }
+    result = (flag===1?'1':'') + result
+    return result
+}
+    let num1 = "9007199254740991"
+    let num2 = "1229007199254740993443"
+    console.log(add(num1, num2))
