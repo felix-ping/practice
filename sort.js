5461585b4e487373064ef8c646b6fcb285bee03d
ping <513011631@qq.com>
2019-04-23 22:53:40 +0800

    3种排序

--- sort.js
+++ sort.js
@@ -1,24 +1,63 @@
 function mergeSort(arr) {
-    if (arr.length < 2) { return arr }
-    let middle = Math.floor(arr.length / 2)
-    let left = arr.slice(0, middle)
-    let right = arr.slice(middle)
-    return merge(mergeSort(left), mergeSort(right))
+	if (arr.length < 2) { return arr }
+	let middle = Math.floor(arr.length / 2)
+	let left = arr.slice(0, middle)
+	let right = arr.slice(middle)
+	return merge(mergeSort(left), mergeSort(right))
 }
 function merge(left, right) {
-    let result = []
-    while (left.length > 0 && right.length > 0) {
-        left[0] >= right[0] ? result.push(right.shift()) : result.push(left.shift())
-    }
-    if (left.length) {
-        result.push(...left)
-    }
-    if (right.length) {
-        result.push(...right)
-    }
-    return result
+	let result = []
+	while (left.length > 0 && right.length > 0) {
+		left[0] >= right[0] ? result.push(right.shift()) : result.push(left.shift())
+	}
+	if (left.length) {
+		result.push(...left)
+	}
+	if (right.length) {
+		result.push(...right)
+	}
+	return result
 }
-
-
 let arr = [7, 8, 9, 4, 2, 5, 4, 1, 6, 8, 22, 45, 98, 1, 0, 6, 7, 8, 9]
 console.log(mergeSort(arr))
+
+
+
+
+
+const quickSort = (arr) => {
+	if (arr.length < 2) { return arr }
+	const basic = arr[0], left = [], right = []
+	for (let i = 1; i < arr.length; i++) {
+		const value = arr[i]
+		value < basic ? left.push(value) : right.push(value)
+	}
+	console.log(left, basic, right)
+	return quickSort(left).concat(basic, quickSort(right))
+}
+let arr = [7, 8, 9, 4, 2, 5, 4, 1, 6, 8, 22, 45, 98, 1, 0, 6, 7, 8, 9]
+console.log(quickSort(arr))
+
+
+
+
+
+const countSort = (arr) => {
+	const count = [], newArr = []
+	for (let i = 0; i < arr.length; i++) {
+		let key = arr[i]
+		count[key] >= 1 ? count[key]++ : count[key] = 1
+	}
+	for (let j = 0; j < count.length; j++) {
+		if (count[j]) {
+			while (count[j] > 0) {
+				newArr.push(j)
+				count[j]--
+			}
+		}
+	}
+	return newArr
+}
+let arr = [7, 8, 9, 4, 2, 5, 4, 1, , 6, 8, 22, 45, 98, 1, 0, 6, 7, 8, 9]
+console.log(countSort(arr))
+
