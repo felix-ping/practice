7259827e223857324907df24313c31e874c89ae3
ping <513011631@qq.com>
2019-05-24 23:51:41 +0800

    html5 优化seo

--- practices.js
+++ practices.js
@@ -2256,3 +2256,24 @@ Array.prototype.mymap = function(callback){
     return arr;
 }
 
+// html5哪些标签可以优化SEO?
+/*跟标签语义化有关，从上往下：
+
+meta
+title
+
+nav
+header
+main
+article
+section
+aside
+footer
+
+figure
+picture
+time
+video
+audio
+*/
+
