e3e91871c873c0bac99512b963f361e48acf1165
ping <513011631@qq.com>
2019-06-23 23:52:24 +0800

    二分查找

--- mianshi.js
+++ mianshi.js
@@ -513,3 +513,29 @@ for ( let item of result) {
 }
 
 console.log(xxx);*/
+
+
+/*
+二分查找
+function Find(target, array) {
+    let i = 0;
+    let j = array[i].length - 1;
+    while (i < array.length && j >= 0) {
+        if (array[i][j] < target) {
+            i++;
+        } else if (array[i][j] > target) {
+            j--;
+        } else {
+            return true;
+        }
+    }
+    return false;
+}
+
+//测试用例
+console.log(Find(10, [
+    [1, 2, 3, 4],
+    [5, 9, 10, 11],
+    [13, 20, 21, 23]
+    ])
+);*/
