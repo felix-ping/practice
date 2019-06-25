13d9425e161d3fd812a5c32acab4c359b91805cc
ping <513011631@qq.com>
2019-06-25 23:58:22 +0800

    每日一题

--- mianshi.js
+++ mianshi.js
@@ -547,3 +547,18 @@ numbers[10] = 11;
 console.log(numbers);
 //[1, 2, 3, 7 x empty, 11]
 */
+
+
+
+/*const a = {};
+const b = { key: "b" };
+const c = { key: "c" };
+
+a[b] = 123;
+a[c] = 456;
+
+console.log(a[b]);
+//456
+a中b,c均为[Object,object]
+
+*/
