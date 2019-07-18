677f193c3e4b3f1037fb216ce63c9d885eb528ae
ping <513011631@qq.com>
2019-07-18 20:14:01 +0800

    每日一题 92th

--- 100.js
+++ 100.js
@@ -5,4 +5,57 @@ var arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 
 var arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 10];
 [...new Set(arr.flat(Infinity))].sort((a,b)=>{return a-b})
 */
+/*
+第 92 题：已知数据格式，实现一个函数 fn 找出链条中所有的父级 id
+const value = '112'
+const fn = (value) => {
+...
+}
 
+const data = [{
+    id: '1',
+    name: 'test1',
+    children: [
+        {
+            id: '11',
+            name: 'test11',
+            children: [
+                {
+                    id: '111',
+                    name: 'test111'
+                },
+                {
+                    id: '112',
+                    name: 'test112'
+                }
+            ]
+
+        },
+        {
+            id: '12',
+            name: 'test12',
+            children: [
+                {
+                    id: '121',
+                    name: 'test121'
+                },
+                {
+                    id: '122',
+                    name: 'test122'
+                }
+            ]
+        }
+    ]
+}];
+fn(value) // 输出 [1， 11， 112]
+
+
+function fn(id, list) {
+  const match = list.find(item => item.id === id);
+  if (match) return [id];
+  const sub = list.find(item => id.startsWith(item.id));
+  return [sub.id].concat(fn(id, sub.children));
+}
+
+
+*/
