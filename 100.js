95f97d1d24c1a0097d70ad0b0092cd16520e1b87
ping <513011631@qq.com>
2019-07-21 22:31:26 +0800

    每日一题 30th 数组合并 解法5,6

--- 100.js
+++ 100.js
@@ -202,6 +202,27 @@ console.log(
       )
     )
 );
+
+var arr1 = ["A1", "A2", "B1", "B2", "C1", "C2", "D1", "D2"];
+var arr2 = ["A", "B", "C", "D"];
+
+arr2.forEach((it, index) => {
+  arr1.splice((index + 1) * 2 + index, 0, it);
+});
+
+console.log(arr1);
+
+let a1 =  ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2']
+let a2 = ['A', 'B', 'C', 'D'].map((item) => {
+  return item + 3
+})
+
+let a3 = [...a1, ...a2].sort().map((item) => {
+  if(item.includes('3')){
+    return item.split('')[0]
+  }
+  return item
+})
 */
 
 
