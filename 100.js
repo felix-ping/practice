ed1f4ebf94f601240e197d01fe172a7d3d2ef0a2
ping <513011631@qq.com>
2019-07-21 22:24:50 +0800

    每日一题 30th 数组合并 解法1

--- 100.js
+++ 100.js
@@ -156,7 +156,18 @@ template(tpl, data); // '<p>This is some text</p>'
 
 /*第 30 题：两个数组合并成一个数组
 请把两个数组 ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2'] 和 ['A', 'B', 'C', 'D']，合并为 ['A1', 'A2', 'A', 'B1', 'B2', 'B', 'C1', 'C2', 'C', 'D1', 'D2', 'D']。
-
+let arr1 = ["A1", "A2", "B1", "B2", "C1", "C2", "D1", "D2"];
+let arr2 = ["A", "B", "C", "D"];
+console.log(
+  [...arr1, ...arr2]
+    .sort(
+      (v2, v1) => (
+        v2.codePointAt(0) - v1.codePointAt(0) ||
+        v1.length - v2.length ||
+        v2.codePointAt(1) - v1.codePointAt(1)
+      )
+    )
+);
 */
 
 
