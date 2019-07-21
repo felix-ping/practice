955840a049f2eeb0853d47ba3a3c0c06d3cb884b
ping <513011631@qq.com>
2019-07-21 22:31:46 +0800

    每日一题 30th 数组合并 解法7

--- 100.js
+++ 100.js
@@ -223,6 +223,26 @@ let a3 = [...a1, ...a2].sort().map((item) => {
   }
   return item
 })
+
+
+const arr1 = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2']
+const arr2 = ['A', 'B', 'C', 'D']
+const ret = []
+let tmp = arr2[0]
+let j = 0
+for (let i=0;i<arr1.length;i++) {
+  if (tmp === arr1[i].charAt(0)){
+    ret.push(arr1[i])
+  }else {
+    ret.push(tmp)
+    ret.push(arr1[i])
+    tmp=arr2[++j]
+  }
+   if(i===arr1.length-1){
+      ret.push(tmp)
+    }
+}
+console.log(ret)
 */
 
 
