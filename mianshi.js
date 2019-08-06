f83d736e17231cd1ada2bda10f37f4777e75b417
ping <513011631@qq.com>
2019-08-06 21:03:25 +0800

    变态async2

--- mianshi.js
+++ mianshi.js
@@ -914,3 +914,45 @@ new Promise(function(resolve) {
     console.log('promise3');
 });
 console.log('script end');*/
+
+
+async function a1() {
+  console.log('a1 start');
+  await a2()
+  console.log('ai end');
+}
+async  function a2() {
+  console.log('a2');
+}
+
+console.log('script start');
+setTimeout(()=>{
+  console.log('setTimeout');
+},0)
+Promise.resolve().then(()=>{
+  console.log('promise1');
+})
+a1()
+let promise2 =new Promise(resolve => {
+  resolve('promise2.then')
+  console.log('promise2');
+})
+promise2.then((res)=>{
+  console.log(res);
+  Promise.resolve().then(()=>{
+    console.log('promise3')
+  })
+})
+console.log('script end');
+//script start
+//a1 start
+//a2
+// promise2
+// end
+
+//下面这俩货有问题
+// a1 end    fsd
+// promise 1  sdfd
+//promise2.then
+//promise3
+//settimeout
