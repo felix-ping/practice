595dbe37377129e73b81ff4d6bca4050dc8392a0
ping <513011631@qq.com>
2019-08-10 23:49:08 +0800

    箭头函数

--- 100.js
+++ 100.js
@@ -614,3 +614,26 @@ node_modules
 
 
 * */
+
+
+/*
+* 箭头函数的情况：
+箭头函数没有自己的this，继承外层上下文绑定的this。
+
+let obj = {
+    age: 20,
+    info: function() {
+        return () => {
+            console.log(this.age); //this继承的是外层上下文绑定的this
+        }
+    }
+}
+
+let person = {age: 28};
+let info = obj.info();
+info(); //20
+
+let info2 = obj.info.call(person);
+info2(); //28
+*
+* */
