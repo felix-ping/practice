ea896b87238927db08df68fef539e105466ede27
ping <513011631@qq.com>
2019-08-23 23:57:51 +0800

    35th

--- 100.js
+++ 100.js
@@ -886,3 +886,14 @@ opacity: 0 ： 修改元素会造成重绘，性能消耗较少
 *
 *
 * */
+
+
+/*第 35 题：浏览器缓存读取规则
+可以分成 Service Worker、Memory Cache、Disk Cache 和 Push Cache，那请求的时候 from memory cache 和 from disk cache 的依据是什么，哪些数据什么时候存放在 Memory Cache 和 Disk Cache中
+
+
+
+*
+*
+*
+* */
