286d2f0ef03aa7e23d885cd290b0afdde653bb97
ping <513011631@qq.com>
2019-11-17 23:56:07 +0800

    61th token

--- 100.js
+++ 100.js
@@ -1663,7 +1663,10 @@ document.getElementsByTagName("img")[0].setAttribute("style","width:300px!import
 *
 * */
 /*61th  介绍下如何实现 token 加密
-*
+*需要一个secret（随机数）
+后端利用secret和加密算法(如：HMAC-SHA256)对payload(如账号密码)生成一个字符串(token)，返回前端
+前端每次request在header中带上token
+后端用同样的算法解密
 *
 * */
 /*第 63 题：如何设计实现无缝轮播
