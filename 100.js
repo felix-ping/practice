5488d6a5d32a0827ed58b69730d098d7c3a9c45b
ping <513011631@qq.com>
2019-12-03 23:56:33 +0800

    71th code

--- 100.js
+++ 100.js
@@ -1782,8 +1782,24 @@ return /[a-z]/.test(item) ? item.toUpperCase() : item.toLowerCase();
 /* 70th 介绍下 webpack 热更新原理，是如何做到在不刷新浏览器的前提下更新页面的*/
 /* 71th 实现一个字符串匹配算法，从长度为 n 的字符串 S 中，查找是否存在字符串 T，T 的长度是 m，若存在返回所在位置
 *
+*const find = (S, T) => {
+  if (S.length < T.length) return -1
+  for (let i = 0; i < S.length; i++) {
+    if (S.slice(i, i + T.length) === T) return i
+  }
+  return -1
+}
 *
 *
+*
+* // 因为 T 的 length 是一定的，所以在循环S的的时候 ，循环当前项 i 后面至少还有 T.length 个元素
+const find = (S, T) => {
+  if (S.length < T.length) return -1;
+  for (let i = 0; i < S.length - T.length ; i++) {
+      if (S.substr(i, T.length) === T) return i ;
+  };
+  return -1;
+};
 * */
 /*第 72 题： 为什么普通 for 循环的性能远远高于 forEach 的性能，请解释其中的原因
 *for 循环没有任何额外的函数调用栈和上下文；
