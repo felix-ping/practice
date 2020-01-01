b811383f5572d64bb849993ab969a467834bd5da
ping <513011631@qq.com>
2020-01-01 23:50:20 +0800

    80th promise.all

--- 100.js
+++ 100.js
@@ -2000,6 +2000,68 @@ ref="input"
 *
 *
 * */
+/*80th 介绍下 Promise.all 使用、原理实现及错误处理
+*  原理实现
+*  function promiseAll(promises){
+     return new Promise(function(resolve,reject){
+            if(!Array.isArray(promises)){
+             return reject(new TypeError("argument must be anarray"))
+           }
+    var countNum=0;
+    var promiseNum=promises.length;
+    var resolvedvalue=new Array(promiseNum);
+    for(var i=0;i<promiseNum;i++){
+      (function(i){
+         Promise.resolve(promises[i]).then(function(value){
+            countNum++;
+           resolvedvalue[i]=value;
+          if(countNum===promiseNum){
+              return resolve(resolvedvalue)
+          }
+       },function(reason){
+        return reject(reason)
+      )
+     })(i)
+    }
+})
+}
+var p1=Promise.resolve(1),
+p2=Promise.resolve(2),
+p3=Promise.resolve(3);
+promiseAll([p1,p2,p3]).then(function(value){
+console.log(value)
+})
+
+
+当promise捕获到error 的时候，代码吃掉这个异常，返回resolve，约定特殊格式表示这个调用成功了
+var p1 =new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve(1);
+    },0)
+});
+var p2 = new Promise(function(resolve,reject){
+        setTimeout(function(){
+            resolve(2);
+        },200)
+ });
+ var p3 = new Promise(function(resolve,reject){
+        setTimeout(function(){
+            try{
+            console.log(XX.BBB);
+            }
+            catch(exp){
+                resolve("error");
+            }
+        },100)
+});
+Promise.all([p1, p2, p3]).then(function (results) {
+    console.log("success")
+     console.log(results);
+}).catch(function(r){
+    console.log("err");
+    console.log(r);
+});
+* */
 /*第 87 题：在输入框中如何判断输入的是一个正确的网址
 
 
