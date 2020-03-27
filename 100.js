475a7d94a6bca571bba4fc5aa54afeb9246d2f21
felix-ping <felix-ping@qq.com>
2020-03-27 22:38:01 +0800

    93th 算法题

--- 100.js
+++ 100.js
@@ -2222,7 +2222,7 @@ const isUrl = urlStr => {
     }, []);
     return search;
   }
-  
+
   const data = ["上海野生动物园", "上饶野生动物园", "北京巷子", "上海中心", "上海黄埔江", "迪士尼上海", "陆家嘴上海中心"]
   const container = document.querySelector('.container');
   const memorizeInput = memorize(handleInput);
@@ -2352,6 +2352,81 @@ console.log(find(2)); // [1, 2]
 console.log(find(3)); // [1, 2, 3]
 console.log(find(4)); // [1, 2, 4]
 * */
+
+
+
+/*93th 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。请找出这两个有序数组的中位数。要求算法的时间复杂度为 O(log(m+n))。
+nums1 = [1, 3]
+nums2 = [2]
+中位数是 2.0
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+中位数是(2 + 3) / 2 = 2.5
+
+答：
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+
+var findMedianSortedArrays = function(nums1, nums2) {
+  let m = nums1.length
+  let n = nums2.length
+  let k1 = Math.floor((m + n + 1) / 2)
+  let k2 = Math.floor((m + n + 2) / 2)
+
+  return (findMedianSortedArraysCore(nums1, 0, nums2, 0, k1) + findMedianSortedArraysCore(nums1, 0, nums2, 0, k2)) / 2
+};
+
+/**
+ * 
+ * @param {number[]} nums1 
+ * @param {number[]} nums2 
+ * @param {number} i 
+ * @param {number} j 
+ * @param {number} k 
+ * @return {number}
+ */
+const findMedianSortedArraysCore = (nums1, i, nums2, j, k)  => {
+  // 如果数组起始位置已经大于数组长度-1
+  // 说明已经是个空数组
+  // 直接从另外一个数组里取第k个数即可
+  if (i > nums1.length - 1) {
+    return nums2[j + k - 1]
+  }
+  if (j > nums2.length - 1) {
+    return nums1[i + k - 1]
+  }
+  // 如果k为1
+  // 就是取两个数组的起始值里的最小值
+  if (k === 1) {
+    return Math.min(nums1[i], nums2[j])
+  }
+  // 取k2为(k/2)或者数组1的长度或者数组2的长度的最小值
+  // 这一步可以避免k2大于某个数组的长度（长度为从起始坐标到结尾）
+  let k2 = Math.floor(k / 2)
+  let length1 = nums1.length - i
+  let length2 = nums2.length - j
+  k2 = Math.min(k2, length1, length2)
+
+  let value1 = nums1[i + k2 - 1]
+  let value2 = nums2[j + k2 - 1]
+
+  // 比较两个数组的起始坐标的值
+  // 如果value1小于value2
+  // 就舍弃nums1前i + k2部分
+  // 否则舍弃nums2前j + k2部分
+  if (value1 < value2) {
+    return findMedianSortedArraysCore(nums1, i + k2, nums2, j, k - k2)
+  } else {
+    return findMedianSortedArraysCore(nums1, i, nums2, j + k2, k - k2)
+  }
+}
+
+
+
+*/ 
 /*99th
 *
 * function fun(num){
@@ -2536,19 +2611,19 @@ Function.prototype.call = function() {
 
 /*console.log('1');
 async function async1(){
-     console.log('2');
-     await console.log('3');
-     console.log('4');
+    console.log('2');
+    await console.log('3');
+    console.log('4');
 }
 setTimeout(function(){
-     console.log('5');
+    console.log('5');
 },0);
 async1();
 new Promise(function(resolve){
-     console.log('6');
-     resolve();
+    console.log('6');
+    resolve();
 }).then(function(){
-     console.log('7');
+    console.log('7');
 });
 console.log('8');
 
