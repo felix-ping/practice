de5923c980a493b988eda2ca2781d111c33bb38a
felix-ping <felix-ping@qq.com>
2020-05-25 22:23:48 +0800

    addEventListener

--- yideng.js
+++ yideng.js
@@ -50,26 +50,72 @@ function isEven2(num){
   }
   return isEven2(num - 2)
 } */
-
+/* 
 function captureOne(re, str){
   var match = re.exec(str)
-  console.log(`captureOne -> match`, match)
+  console.log(`match`, match , re.lastIndex)
   return match && match[1]
 }
 var numRe = /num=(\d+)/ig
 var wordRe = /yideng=(\w+)/i
-var a1 = captureOne(numRe, "num=1") //1
+var a1 = captureOne(numRe, "num=11") //1   lastIndex = 6
 var a2 = captureOne(wordRe, "yideng=1") //1
-var a3 = captureOne(numRe, "NUM=2")//null
+
 var a4 = captureOne(wordRe, "YIDENG=2")//2
-var a5 = captureOne(numRe, "Num=3")//3
+
 var a6 = captureOne(wordRe, "YiDeng=3")//3
+
+
+var a3 = captureOne(numRe, "NUM=22")//null
+var a5 = captureOne(numRe, "Num=33")//3
+var a6 = captureOne(numRe, "Num=44")//null
+
+//所以问题出在正则的/g上  ， 
+//当执行完一次/g后  lastIndex 在执行的后一位置，并不会重置。下次执行时会从这一位置开始，找不到返回null，重置lastIndex=0
+
 console.log(a1 === a2)
 console.log(a3 === a4)
-console.log(a5 === a6)
-
-
-
-
+console.log(a5 === a6) */
 //T F T
 //
+
+
+((w) => {
+  let x = w.addEventListener;
+  let y = new Set()
+  w.addEventListener = function (event, fn) {
+    x.call(w, event, fn)
+    y.add(event)
+    console.log(y)
+  }
+})(window);
+
+
+
+((w)=>{
+  w.listenerList = new Set()
+  const _cache = w.addEventListener
+  const handler = { 
+    apply: (target, thisBinding, args)=>{
+      target()
+      listenerList.add(args[0])
+      Reflect.apply(_cache, w, args)
+    }
+  }
+  w.addEventListener = new Proxy((eventName, fn)=>{
+    console.log('proxy')
+  },handler)
+})(window)
+
+
+
+window.addEventListener('click', () => {
+  console.log(1)
+})
+
+window.addEventListener('resize', () => {
+  console.log(2)
+})
+window.addEventListener('click', () => {
+  console.log(this)
+})
